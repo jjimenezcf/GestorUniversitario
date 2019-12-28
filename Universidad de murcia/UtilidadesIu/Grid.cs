cd13a878ba6c33ad30cb7f8e83f06e74b12cca71
jjimenezcf <56802725+jjimenezcf@users.noreply.github.com>
2019-12-28 12:09:27 +0100

    Crear clase tabulator

--- Universidad de murcia/UtilidadesIu/Grid.cs
+++ Universidad de murcia/UtilidadesIu/Grid.cs
@@ -6,11 +6,6 @@ namespace UtilidadesParaIu
 {
     public class Grid
     {
-        const string _alPulsarPaginaSiguiente = "paginaSiguiente(controlador,accion,registrosPorleer,ultLeido)";
-        const string _alPulsarPaginaAnterior = "paginaSiguiente(controlador,accion,registrosPorleer,ultLeido)";
-        const string _alPulsarPaginaUltima = "paginaSiguiente(controlador,accion,registrosPorleer,ultLeido)";
-        const string _alPulsarPaginaInicial = "paginaSiguiente(controlador,accion,registrosPorleer,ultLeido)";
-
         public string Id { get; private set; }
         public string Ruta { get; set; }
         public List<ColumnaDelGrid> columnas { get; private set; }
@@ -71,26 +66,6 @@ namespace UtilidadesParaIu
             //{ title: ¨Name¨, field: ¨name¨, sorter: ¨string¨, width: 200, editor: true },
         }
 
-        private static string TabuladorColumnaCabecera(ColumnaDelGrid columna)
-        {
-            var ancho = columna.Ancho > 0 ? $", width: {columna.Ancho} " : "";
-            var visible = $", visible: {columna.Visible.ToString().ToLower()} ";
-            var editable = $", editor: {columna.Editable.ToString().ToLower()} ";
-            var sorter = columna.Ordenar ?  $", sorter: ¨{columna.Tipo.Name.ToLower()}¨ ": "";
-            var alineacion = $", align:¨{columna.Alineacion()}¨";
-
-            var descriptor = "{title: ¨Titulo¨, field: ¨Nombre¨{sorter}{ancho}{visible}{editable}{alineacion}}";
-            descriptor = descriptor
-               .Replace("Titulo", columna.Titulo)
-               .Replace("Nombre", columna.Nombre)
-               .Replace("{sorter}", sorter)
-               .Replace("{ancho}", ancho)
-               .Replace("{visible}", visible)
-               .Replace("{editable}", editable)
-               .Replace("{alineacion}", alineacion);
-
-            return $"{Environment.NewLine}{descriptor},";
-        }
 
         private static string RenderCelda(CeldaDelGrid celda)
         {
@@ -98,15 +73,15 @@ namespace UtilidadesParaIu
             return $"<td id=¨{celda.Id}¨ name=¨{celda.IdCabecera}¨ class=¨{celda.AlineacionCss()}¨ {ocultar}>{celda.Valor}</td>";
         }
 
-        private static string RenderFila(int numFil, FilaDelGrid filaDelGrid)
+        private static string RenderFila(int numFil, FilaDelGrid fila)
         {
-            var fila = new StringBuilder();
-            foreach (var celda in filaDelGrid.Celdas)
+            var filaHtml = new StringBuilder();
+            foreach (var celda in fila.Celdas)
             {
                 celda.Id = $"{celda.IdCabecera}_{numFil}";
-                fila.AppendLine(RenderCelda(celda));
+                filaHtml.AppendLine(RenderCelda(celda));
             }
-            return $@"{fila.ToString()}";
+            return $@"{filaHtml.ToString()}";
         }
 
         private static string RenderFilaSeleccionable(string idGrid, int numFil, FilaDelGrid filaDelGrid)
@@ -129,17 +104,6 @@ namespace UtilidadesParaIu
             return $@"<thead>{Environment.NewLine}<tr>{cabeceraHtml.ToString()}{Environment.NewLine}</tr>{Environment.NewLine}</thead>";
         }
 
-        private static string TabuladorCabecera(string idGrid, IEnumerable<ColumnaDelGrid> columnasGrid)
-        {
-            var tabuladorCabecera = new StringBuilder();
-            foreach (var columna in columnasGrid)
-            {
-                columna.Id = $"{idGrid}_{columna.Nombre}";
-                tabuladorCabecera.Append(TabuladorColumnaCabecera(columna));
-            }
-            
-            return $@"{tabuladorCabecera.ToString()}{Environment.NewLine}";
-        }
 
         private static string RenderDetalleGrid(string idGrid, IEnumerable<FilaDelGrid> filas)
         {
@@ -158,15 +122,15 @@ namespace UtilidadesParaIu
             var htmlNavegadorGrid = $@"
             <div id=¨Nav-{grid.Id}¨>
                 <div id=¨Nav-{grid.Id}-1¨ data-type=¨img¨ style=¨display:inline-block¨>
-                    <img src=¨/images/paginaInicial.png¨ alt=¨Primera página¨ title=¨Ir al primer registro¨ width=¨22¨ height=¨22¨ onclick=¨Leer('{grid.Ruta}')¨>
+                    <img src=¨/images/paginaInicial.png¨ alt=¨Primera página¨ title=¨Ir al primer registro¨ width=¨22¨ height=¨22¨ onclick=¨Leer('{grid.Id}','{grid.Ruta}')¨>
                 </div>
                 <div id=¨Nav-{grid.Id}-2¨ class=¨mx-sm-3¨ style=¨display:inline-block¨>
                     <input type=¨number¨ id=¨Nav-{grid.Id}-Reg¨ value=¨10¨ min=¨5¨ step=¨5¨ max=¨999¨ style=¨width: 50px;margin-top: 5px;align-content:center; border-radius: 10px¨>
                 </div>
                 <div id=¨Nav-{grid.Id}-3¨ data-type=¨img¨ style=¨display:inline-block¨>
-                    <img src=¨/images/paginaAnterior.png¨ alt=¨Primera página¨ title=¨Ir al primer registro¨ width=¨22¨ height=¨22¨ onclick=¨LeerAnteriores('{grid.Ruta}')¨>
-                    <img src=¨/images/paginaSiguiente.png¨ alt=¨Siguiente página¨ title=¨Ir al primer registro¨ width=¨22¨ height=¨22¨ onclick=¨LeerSiguientes('{grid.Ruta}')¨>
-                    <img src=¨/images/paginaUltima.png¨ alt=¨Última página¨ title=¨Ir al primer registro¨ width=¨22¨ height=¨22¨ onclick=¨LeerUltimos('{grid.Ruta}')¨>
+                    <img src=¨/images/paginaAnterior.png¨ alt=¨Primera página¨ title=¨Ir al primer registro¨ width=¨22¨ height=¨22¨ onclick=¨LeerAnteriores('{grid.Id}','{grid.Ruta}')¨>
+                    <img src=¨/images/paginaSiguiente.png¨ alt=¨Siguiente página¨ title=¨Ir al primer registro¨ width=¨22¨ height=¨22¨ onclick=¨LeerSiguientes('{grid.Id}','{grid.Ruta}')¨>
+                    <img src=¨/images/paginaUltima.png¨ alt=¨Última página¨ title=¨Ir al primer registro¨ width=¨22¨ height=¨22¨ onclick=¨LeerUltimos('{grid.Id}','{grid.Ruta}')¨>
                 </div>
             </div>
             ";
@@ -181,28 +145,9 @@ namespace UtilidadesParaIu
 
         private static string RenderizarGrid(Grid grid)
         {
-            var tabulator = $@"
-            <div id=¨{grid.Id}¨>
-            </div>";
-
-            var seleccionable = $"selectable: {grid.ConSeleccion.ToString().ToLower()}";
-            
-            var crearTabulator = @"
-            <script>
-                var table = new Tabulator(¨#{grid.Id}¨, {
-                    layout:¨fitColumns¨,
-                    {seleccionable},
-                    columns: [{columnas}],
-                });
-            </script>
-            "
-            .Replace("{grid.Id}", grid.Id)
-            .Replace("{seleccionable}", seleccionable)
-            .Replace("{columnas}", TabuladorCabecera(grid.Id, grid.columnas));
-
             var htmlTabla = $"<table id=¨{grid.Id}¨ class=¨table table-striped table-hover¨ width=¨100%¨>{Environment.NewLine}{RenderCabecera(grid.Id, grid.columnas)}{Environment.NewLine}{RenderDetalleGrid(grid.Id, grid.filas)}</table>";
             var htmlNavegador = grid.ConNavegador ? RenderNavegadorGrid(grid) : "";
-            return $"{tabulator}{Environment.NewLine}{crearTabulator}" + (htmlTabla + htmlNavegador + RenderOpcionesGrid());
+            return (htmlTabla + htmlNavegador + RenderOpcionesGrid());
         }
 
     }
