099ca512a467a611506d962d7c2e67977b9c8f4a
Juan Jiménez-Cervantes Frigols <jjimenezcf@gmail.com>
2020-02-05 20:20:27 +0100

    Empezar a definir el crud de estudiantes

---
+++ Universidad de murcia/Descriptores/CrudEstudiante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gestor.Elementos.Universitario.ModeloIu;
+
+namespace UniversidadDeMurcia.Descriptores
+{
+    public class CrudEstudiante: DescriptorDeCrud<ElementoEstudiante>
+    {
+        public CrudEstudiante()
+        :base()
+        {
+           var b = new Bloque($"{Filtro.Id}_b3", "Específico", new Dimension(1, 4));
+           b.Controles.Add(new Selector("SelectorCurso"));
+           Filtro.Add(b);            
+        }
+    }
+}
--- Universidad de murcia/Descriptores/DescriptorDeCrud.cs
+++ Universidad de murcia/Descriptores/DescriptorDeCrud.cs
@@ -2,37 +2,105 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Gestor.Elementos.ModeloIu;
 
 namespace UniversidadDeMurcia.Descriptores
 {
-
-    public class EstudianteDescriptorDeCrud: DescriptorDeCrud
+    public class Posicion
     {
-        public EstudianteDescriptorDeCrud():
-        base("Estudiante")
+        int fila { get; set; }
+        int columna { get; set; }
+    }
+
+    public class Dimension
+    {
+        public int Filas { get; private set; }
+        public int Columnas { get; private set; }
+
+        public Dimension(int filas, int columnas)
         {
-            Filtro = new ZonaDeFiltro();
-            var bloque = new Bloque { nombre = "Específico", tablaBloque = new TablaBloque { columnas = 4, filas = 1 }, controles = new List<Control>() };
-            Filtro.Bloques.Add(bloque);
+            Filas = filas;
+            Columnas = columnas;
         }
     }
 
-    public class DescriptorDeCrud
+    public class Columnas
     {
-        public string Mantenimiento { get; set; }
-        public ZonaDeFiltro Filtro { get; set; }
-        public ZonaDeGrid Grid { get; set; }
-        public ZonaDeOpciones Opciones { get; set; }
+        public string etiqueta { get; set; }
+        public string propiedad { get; set; }
+        public string visible { get; set; }
+        public string tipo { get; set; }
+    }
 
-        public DescriptorDeCrud()
+    public class Control
+    {
+        public string id { get; set; }
+        public string tipo { get; set; }
+        public string etiqueta { get; set; }
+        public string propiedad { get; set; }
+        public string ayuda { get; set; }
+        public Posicion posicion { get; set; }
+        public ICollection<Valor> valores { get; set; }
+    }
+
+    public class Selector : Control
+    {
+        public string propiedadParaFiltrar { get; set; } = "Id";
+        public string propiedadParaMostrar { get; set; }
+        public PanelDeSeleccion modal { get; set; }
+
+        public Selector(string idSelector)
         {
-            Mantenimiento = "Estudiante";
-            Filtro = new ZonaDeFiltro();
+            tipo = nameof(Selector);
+            id = idSelector;
+        }
+    }
+
+    public class PanelDeSeleccion : Control
+    {
+        public Selector selector { get; set; }
+        public string gestorDeElementos { get; set; }
+        public string claseDeElemento { get; set; }
+        public ICollection<Columnas> columnasDelGrid { get; set; }
+        public string Registros { get; set; }
+
+        public PanelDeSeleccion(Selector selectorAsociado)
+        {
+            selector = selectorAsociado;
+            selector.modal = this;
+        }
+    }
+    
+    public class TablaBloque
+    {
+        public string Id { get; private set; }
+        public Dimension Dimension { get; private set; }
+        public ICollection<Control> Controles { get; set; }
+
+        public TablaBloque(string identificador, Dimension dimension, ICollection<Control> controles)
+        {
+            Id = identificador;
+            Dimension = dimension;
+            Controles = controles;
         }
 
-        public DescriptorDeCrud(string nombre)
+    }
+
+    public class Bloque
+    {
+        public string Id { get; private set; }
+
+        public string Titulo { get; private set; }
+
+        public TablaBloque Tabla { get; set; }
+
+        public ICollection<Control> Controles => Tabla.Controles;
+
+        public Bloque(string identificador, string titulo, Dimension dimension)
         {
-            Mantenimiento = nombre;
+            Id = identificador;
+            Titulo = titulo;
+            Tabla = new TablaBloque($"{Id}_Tbl", dimension, new List<Control>());
         }
     }
 
@@ -43,73 +111,71 @@ namespace UniversidadDeMurcia.Descriptores
 
     public class ZonaDeGrid
     {
-        public ICollection<Columnasdelgrid> columnas { get; set; }
+        public ICollection<Columnas> columnas { get; set; }
         public string Registros { get; set; }
     }
 
     public class ZonaDeFiltro
     {
-        public ICollection<Bloque>  Bloques { get; set; }
+        public ICollection<Bloque> Bloques { get; private set; } = new List<Bloque>();
 
-        public ZonaDeFiltro()
+        public string Id  { get; private set; }
+
+        public ZonaDeFiltro(string identificador)
         {
-            Bloques = new List<Bloque>
+            Id = $"flt_{identificador}";
+
+            var b1 = new Bloque($"{Id}_b1", "General", new Dimension(1, 4));
+            var b2 = new Bloque($"{Id}_b2", "Común", new Dimension(1, 4));
+
+            Bloques.Add(b1);
+            Bloques.Add(b2);
+        }
+
+        public void Add(Bloque bloque)
+        {
+            Bloques.Add(bloque);
+        }
+
+        public Bloque Get(string identificador)
+        {
+            foreach(Bloque b in Bloques)
             {
-                new Bloque { nombre = "General", tablaBloque = new TablaBloque {columnas = 4 , filas = 1}, controles = new List<Control>()  },
-                new Bloque { nombre = "Común", tablaBloque = new TablaBloque {columnas = 4 , filas = 1}, controles = new List<Control>()  }
-            };
+                if (b.Id == identificador)
+                    return b;
+            }
+
+            throw new Exception($"El bloque {identificador} no está en la zona de filtrado");
         }
     }
 
-    public class TablaBloque
+    public class DescriptorDeCrud<TElemento>
+    where TElemento : ElementoBase
     {
-        public int columnas { get; set; }
-        public int filas { get; set; }
+        public string Elemento { get; private set; }
+        public ZonaDeFiltro Filtro { get; private set; }
+        public ZonaDeGrid Grid { get; set; }
+        public ZonaDeOpciones Opciones { get; set; }
+
+        public DescriptorDeCrud()
+        {
+            Elemento = typeof(TElemento).ToString().Replace("Elemento","");
+            Filtro = new ZonaDeFiltro(Elemento);
+        }
 
     }
-
-    public class Bloque
-    {
-        public string nombre { get; set; }
-        public TablaBloque tablaBloque { get; set; }
-        public ICollection<Control> controles { get; set; }
-    }
-
-    public class Control
-    {
-        public string tipo { get; set; }
-        public string etiqueta { get; set; }
-        public string propiedad { get; set; }
-        public string ayuda { get; set; }
-        public string accion { get; set; }
-        public string fila { get; set; }
-        public string columna { get; set; }
-        public ICollection<Valor> valores { get; set; }
-        public string gestorDeElementos { get; set; }
-        public string claseDeElemento { get; set; }
-        public string propiedadParaFiltrar { get; set; }
-        public string propiedadParaMostrar { get; set; }
-        public Columnasdelgrid[] columnasDelGrid { get; set; }
-        public string Registros { get; set; }
-    }
-
+          
     public class Valor
     {
         public string Nombreestudiante { get; set; }
         public string Fechadeinscripción { get; set; }
     }
 
-    public class Columnasdelgrid
-    {
-        public string etiqueta { get; set; }
-        public string propiedad { get; set; }
-        public string visible { get; set; }
-        public string tipo { get; set; }
-    }
-
+    
     public class Opcion
     {
     }
+
 
 
 
