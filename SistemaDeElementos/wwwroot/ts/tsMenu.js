879a623e47392441efd5503e54cf33f95bdc7399
Juan Jiménez-Cervantes Frigols <jjimenezcf@gmail.com>
2020-03-14 16:46:53 +0100

    Añadir la opción de abrir y cerrar el menú principal

---
+++ SistemaDeElementos/wwwroot/ts/tsMenu.js
@@ -0,0 +1,18 @@
+var Menu;
+(function (Menu) {
+    function Mostrar() {
+        var idProductoHtml = document.getElementById('idproducto');
+        var menuAbierto = idProductoHtml.getAttribute("menuAbierto");
+        if (menuAbierto === undefined || menuAbierto === "false") {
+            idProductoHtml.setAttribute("menuAbierto", "true");
+            document.getElementById("sidebar").style.width = "300px";
+        }
+        else {
+            idProductoHtml.setAttribute("menuAbierto", "false");
+            document.getElementById("sidebar").style.width = "0";
+            document.getElementById("contenido").style.marginLeft = "0";
+        }
+    }
+    Menu.Mostrar = Mostrar;
+})(Menu || (Menu = {}));
+//# sourceMappingURL=tsMenu.js.map
---
+++ SistemaDeElementos/wwwroot/ts/tsMenu.ts
@@ -0,0 +1,15 @@
+﻿module Menu {
+    export function Mostrar() {
+        var idProductoHtml = document.getElementById('idproducto');
+        var menuAbierto = idProductoHtml.getAttribute("menuAbierto");
+        if (menuAbierto === undefined || menuAbierto === "false") {
+            idProductoHtml.setAttribute("menuAbierto", "true");
+            document.getElementById("sidebar").style.width = "300px";
+        }
+        else {
+            idProductoHtml.setAttribute("menuAbierto", "false");
+            document.getElementById("sidebar").style.width = "0";
+            document.getElementById("contenido").style.marginLeft = "0";
+        }
+    }
+}
