dd2fa7595a1fb0bca8112e1538ddfb7c30d464c1
Juan Jiménez-Cervantes Frigols <jjimenezcf@gmail.com>
2020-04-05 20:10:56 +0200

    separar las opciones de menú de la clase de menú

---
+++ SistemaDeElementos/Descriptores/Componentes/Elementos/OpcionDeMenu.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCSistemaDeElementos.Descriptores
+{
+    public enum TipoAccion { MostrarDiv, OcultarDiv }
+    public class AccionDeMenu
+    {
+        public TipoAccion tipoAccion { get; protected set; }
+
+        public virtual string RenderAccion()
+        {
+            return "EjecutarAccionMenu({parametros})";
+        }
+
+    }
+
+    public class MostrarDiv : AccionDeMenu
+    {
+
+        public string IdDivMostrar { get; set; }
+        public string idDivMostrarHtml => IdDivMostrar.ToLower();
+
+        public string IdDivOcultar { get; set; }
+        public string idDivOcultarHtml => IdDivOcultar.ToLower();
+
+        public MostrarDiv(string idDivMostrar, string idDivOcultar)
+        {
+            tipoAccion = TipoAccion.MostrarDiv;
+            this.IdDivMostrar = idDivMostrar;
+            this.IdDivOcultar = idDivOcultar;
+        }
+
+        public override string RenderAccion()
+        {
+            var htmlAccion = base.RenderAccion();
+            return htmlAccion.Replace("{parametros}", $"'{idDivMostrarHtml}','{idDivOcultarHtml}'");
+        }
+
+
+    }
+
+    public class OcultarDiv : AccionDeMenu
+    {
+
+        public string IdDivOcultar { get; set; }
+        public string IdDivOcultarHtml => IdDivOcultar.ToLower();
+
+        public OcultarDiv(string idDivOcultar)
+        {
+            tipoAccion = TipoAccion.OcultarDiv;
+            this.IdDivOcultar = idDivOcultar;
+        }
+
+    }
+    public class OpcionDeMenu<TElemento> : ControlHtml
+    {
+        public Menu<TElemento> Menu => (Menu<TElemento>)Padre;
+        public AccionDeMenu Accion { get; private set; }
+
+        public OpcionDeMenu(Menu<TElemento> menu, AccionDeMenu accion, string titulo)
+        : base(
+          padre: menu,
+          id: $"{menu.Id}_{TipoControl.Opcion}_{menu.OpcioneDeMenu.Count}",
+          etiqueta: titulo,
+          propiedad: null,
+          ayuda: null,
+          posicion: null
+        )
+        {
+            Tipo = TipoControl.Opcion;
+            Accion = accion;
+        }
+
+        public override string RenderControl()
+        {
+            var htmlOpcionMenu = $"<input id=¨{IdHtml}¨ type=¨button¨ value=¨{Etiqueta}¨ onClick=¨{Accion.RenderAccion()}¨>";
+            return htmlOpcionMenu;
+        }
+    }
+}
